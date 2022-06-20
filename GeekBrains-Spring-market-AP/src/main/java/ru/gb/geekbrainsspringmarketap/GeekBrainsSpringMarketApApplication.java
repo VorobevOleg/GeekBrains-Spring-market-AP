4f25a4becb00c077e79f298bf1c623691cae52ed
VorobevOleg <kukuruza-09@ya.ru>
2022-06-20 13:56:29 +0300

    Project created

---
+++ GeekBrains-Spring-market-AP/src/main/java/ru/gb/geekbrainsspringmarketap/GeekBrainsSpringMarketApApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.geekbrainsspringmarketap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekBrainsSpringMarketApApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeekBrainsSpringMarketApApplication.class, args);
+    }
+
+}
---
+++ GeekBrains-Spring-market-AP/src/test/java/ru/gb/geekbrainsspringmarketap/GeekBrainsSpringMarketApApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.geekbrainsspringmarketap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekBrainsSpringMarketApApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
